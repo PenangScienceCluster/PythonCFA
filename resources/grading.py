04cc48a4a25a70a00e213efeabdaffac9bd0746a
Chan Ju Ping <juping@georgetown.technology>
2021-08-04 11:13:53 +0800

    feat: 2021 grading code chunk that can be added to submissions

---
+++ resources/grading.py
@@ -0,0 +1,20 @@
+# Grading Comments
+print("Functionality: ")
+print("Coding: ")
+
+
+## If illogical, 1; if while logic works, 2; if additional improvements made, 3
+print("Evaluation: ")
+
+
+
+print("Elements: ")
+
+#Grading Score
+list = [0, 0, 0, 0]
+
+print(
+    "Total score: " + str(
+        sum(list)
+    )
+)
