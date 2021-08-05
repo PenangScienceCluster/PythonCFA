ec0532e3f9497f4966fdd34fbb579a2b56ea6bf2
Chan Ju Ping <juping@georgetown.technology>
2021-08-05 13:31:53 +0800

    Added example exercises

---
+++ intermediate/scripts/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from src import ana, ahkit
+
+print("------------------")
+print("Project Manager")
+ana.main()
+print("------------------")
+
+print("Team Lead")
+ahkit.ahkit()
